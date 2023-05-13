578bd1792de6b57d827e967ce0c5220cdf49061c
PUSHKAR RAJ <rajpushkar556@gmail.com>
2023-05-13 19:20:40 +0530

    initial commit

---
+++ assets/js/form.js
@@ -0,0 +1,4 @@
+function showAlert() {
+    alert("Your table has been reserved. We look forward to welcoming you!");
+    window.location.href = "index.html"; // Replace "index.html" with the actual URL of your home page
+}
